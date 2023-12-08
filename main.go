f8d6d69d5e5e623050d14af5a7cddde9da64f6fd
Juan Pratama Wahyu Talendra <108502921+juanPWT@users.noreply.github.com>
2023-12-08 19:56:41 +0700

    add: cors

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	messegecontroller "github.com/juanPWT/api-letstalk/controllers/messegeController"
 	"github.com/juanPWT/api-letstalk/models"
 )
@@ -9,6 +10,13 @@ import (
 func main() {
 	models.ConnectToDb()
 	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowHeaders:     "Origin,Content-Type,Accept,Content-Length,Accept-Language,Accept-Encoding,Connection,Access-Control-Allow-Origin",
+		AllowOrigins:     "*",
+		AllowCredentials: true,
+		AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
+	}))
 
 	api := app.Group("/api")
 
